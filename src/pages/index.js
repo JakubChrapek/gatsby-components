fbae1b2df67650631851803cbb68d24d81018c56
jakubchrapek <chrapekuba@gmail.com>
2021-09-25 15:30:56 +0200

    Reverting to clean project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,10 @@
 import React from "react"
-import Card from "../components/card"
-import Layout from "../components/layout"
-import { Wrapper } from "../components/wrapper"
-import cardBg from "../img/sea-landscape-bg.jpg"
 
 export default function Home() {
   return (
-    <Layout>
-      <Wrapper>
-        <Card
-          title="Hotel 123"
-          text="Sem nunc non turpis nunc convallis turpis malesuada iaculis lacinia."
-          tags={["dog friendly", "non smoking", "sea view"]}
-          img={cardBg}
-          imgAlt="Sea landscape"
-        />
-      </Wrapper>
-    </Layout>
+    <>
+      <h1>Wyzwanie UI #01</h1>
+      <p>Ready, steady, go!</p>
+    </>
   )
 }
